c95e916aa06c5cf2423786f4e16947e7a9ad8ff3
fedotovdmitriy14 <fedotovdmitriy14@gmail>
2021-10-10 15:05:52 +0300

    second version

--- task1_postman_routes.py
+++ task1_postman_routes.py
@@ -9,37 +9,37 @@ def postman_routes(routes):
     result = {}
 
     for route in list(perm):
-        counter = 0
+        counter = 1
         total_length = 0
         i = 0
         current_route = ""
-        while counter <= len(route)+2:
-            if counter == 0:
+        while True:
+            if counter == 1:
                 length = math.sqrt((route[i][0] - 0) ** 2 + (route[i][1] - 2) ** 2)
                 total_length += length
                 current_route += f"(0, 1) -> {(route[i][0], route[i][1])}[{total_length}]"
                 counter += 1
                 i += 1
-            elif counter >= 1 and counter <= len(route)+1:
+            elif i < len(route):
                 length = math.sqrt((route[i][0] - route[i-1][0]) ** 2 + (route[i][1] - route[i-1][1]) ** 2)
                 total_length += length
-                current_route += f" -> {(route[i][0], route[i][1])}[{total_length}"
+                current_route += f" -> {(route[i][0], route[i][1])}[{total_length}]"
                 counter += 1
-                if counter < len(route):
-                    i += 1
-            elif counter == len(route)+2:
+                i += 1
+            else:
+                i = len(route) - 1
                 length = math.sqrt((0 - route[i][0]) ** 2 + (2 - route[i][1]) ** 2)
                 total_length += length
                 current_route += f" -> {(0, 1)}[{total_length}]"
                 counter += 1
+                break
+        # print(current_route)
         result[total_length] = current_route
 
 
     min_key = min(result, key=int)
 
+
     return f"{result[min_key]} = {min_key}"
 
 print(postman_routes(routes))
-
-
-
